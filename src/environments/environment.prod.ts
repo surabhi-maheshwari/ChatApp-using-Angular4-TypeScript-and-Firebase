801996a200a34a4678c0c92a94d848c7b9b0ba4f
Surabhi Maheshwari <32718459+surabhi-maheshwari@users.noreply.github.com>
2018-04-17 15:14:51 -0400

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyArX0Cej-yG8BHwb_DmCcVYhuBI2w41lPg',
+    authDomain: 'chat-demo-f81b9.firebaseapp.com',
+    databaseURL: 'https://chat-demo-f81b9.firebaseio.com',
+    projectId: 'chat-demo-f81b9',
+    storageBucket: 'chat-demo-f81b9.appspot.com',
+    messagingSenderId: '265147669223'
+  }
+};
